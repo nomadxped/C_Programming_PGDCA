3aa933ed770538c15f27a80b448d7be6815e3f9a
Anvayavardhana Kaundinyayana <acharyaanvaya@gmail.com>
2026-01-06 15:09:09 +0545

    comment addded

--- 01.c
+++ 01.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
+/*
+#Documentation#
+First Program in C
+Author: Anvaya Acharya
+Date: 01/06/2026
+*/
+#include <stdio.h>  // Link standard input/output library
 
-
+//Main Program
 main(){
-	printf("Anu\n");
+	printf("Anu\n"); //printf("your name");
 
+// Variable declaration
 int i;
 float f;
 char c;
 double d;
 
-printf("Enter any character: ");
-scanf("%c", &c);
+printf("Enter any character: "); 
+scanf("%c", &c); //scanning for the input from user and place the input to the memory location of c
 
 printf("Enter any number: ");
 scanf("%d", &i);
@@ -21,10 +28,13 @@ scanf("%f", &f);
 printf("Enter any number (double): ");
 scanf("%lf", &d);
 
-printf("\n\nyou have entered charcater: %c\n", c);
+
+// Display input values
+printf("\n\nyou have entered charcater: %c\n", c); 
 printf("you have entered number: %d\n", i);
 printf("you have entered number (float): %f\n", f);
 printf("you have entered charcater (double): %lf", d);
 
 }
+
 
